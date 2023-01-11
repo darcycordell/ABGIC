f07786f4243349bdaec018db4ec0a8f52e666175
darcycordell <dcordell@ualberta.ca>
2023-01-11 14:23:22 -0700

    Update calc_E.m
    10000 times faster speedup on calculating E by using pagemtimes

--- calc/calc_E.m
+++ calc/calc_E.m
@@ -16,14 +16,23 @@ mu = 4*pi*10^-7;
 
 Z = reshape(Z(:,[1 3 2 4]).',2,2,size(Bx,2));
 B = [Bx; By]*10^-9; %Conversion from nT to Tesla
-E = zeros(size(B));
 
-parfor i = 1:length(Bx) %Loop through frequencies
-    
-    %E is in V/m. Z is in Ohm. B is in T. B/mu = H in A/m
-    E(:,i) = (1/mu)*(real(Z(:,:,i))*real(B(:,i))+1i*imag(Z(:,:,i))*imag(B(:,i)));
-    
-end
+B = permute(B,[1,3,2]); %Add 2nd dimension for pagemtimes
+
+%E is in V/m. Z is in Ohm. B is in T. B/mu = H in A/m
+%pagemtimes is a very clever way to do the multiplication that avoids a for
+%loop. 10000 times faster.
+
+E = (1/mu)*(pagemtimes(real(Z),real(B))+1i*pagemtimes(imag(Z),imag(B)));
+E = permute(E,[1,3,2]);
+
+
+% parfor i = 1:length(Bx) %Loop through frequencies
+%     
+%     %E is in V/m. Z is in Ohm. B is in T. B/mu = H in A/m
+%     E(:,i) = (1/mu)*(real(Z(:,:,i))*real(B(:,i))+1i*imag(Z(:,:,i))*imag(B(:,i)));
+%     
+% end
     
 
 Ex = E(1,:);

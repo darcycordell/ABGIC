2c712275950978c905d8ee362f57d8427a2f4b2e
darcycordell <dcordell@ualberta.ca>
2025-01-23 08:50:41 -0700

    Update clean_mag_data.m
    Added ability to output b variable directly to workspace rather than saving.
    Added menu to edit out values over a time window or over a range of b values.

--- utils/clean_mag_data.m
+++ utils/clean_mag_data.m
@@ -1,13 +1,13 @@
-function clean_mag_data
+function b = clean_mag_data(b)
 %Data located in 04_MAG_DATA folder
 %%
-[b] = load_mag_data;
+%[b] = load_mag_data;
 
 %Delete duplicates
 b(cellfun(@isempty,({b(:).x}))) = [];
 %%
 % Check for "bad" points
-winlength = 3600;
+winlength = min([b.nt 3600]);
 is = 1;tidx = 1:winlength;%length(b(is).times);
 while 1
     
@@ -16,25 +16,14 @@ while 1
     ylabel('B (nT)')
     title(['Magnetic Time Series for ',upper(b(is).site)]);
     
-    mmenu = menu('','Edit Points','Next Window','Change Window Length','Next Site','Previous Site','Delete Sites','Save','Exit');
+    mmenu = menu('','Edit Points (Value)','Edit Points (Time)','Next Window','Change Window Length','Next Site','Previous Site','Delete Sites','Save','Exit');
     
     if mmenu == 1
     
-    [x,y] = ginput(2);
+    [~,y] = ginput(2);
 
     %if click~=3
         ax = gca;
-
-%Option to cut by time
-%         t1 = num2ruler(x(1),ax.XAxis);
-%         t2 = num2ruler(x(2),ax.XAxis);
-%         [~ ,id1] = min(abs(datenum(t1)-datenum(b(is).times)));
-%         [~ ,id2] = min(abs(datenum(t2)-datenum(b(is).times)));
-% 
-%         b(is).x(id1:id2) = linspace(b(is).x(id1-1),b(is).x(id2+1),length(id1:id2));
-%         b(is).y(id1:id2) = linspace(b(is).y(id1-1),b(is).y(id2+1),length(id1:id2));
-%         b(is).z(id1:id2) = linspace(b(is).z(id1-1),b(is).z(id2+1),length(id1:id2));
-% 
 
 %Option to cut by mag value (faster for removing outliers)
 
@@ -51,13 +40,38 @@ while 1
         b(is).z(id) = NaN;
 
 
-        b(is).x = inpaint_nans(b(is).x);
-        b(is).y = inpaint_nans(b(is).y);
-        b(is).z = inpaint_nans(b(is).z);
+        b(is).x = inpaint_nans(b(is).x,4);
+        b(is).y = inpaint_nans(b(is).y,4);
+        b(is).z = inpaint_nans(b(is).z,4);
+        close(gcf);
+
+    elseif mmenu == 2
+
+        [x,~] = ginput(2);
+
+
+        ax = gca;
+        %Option to cut by time
+        t1 = num2ruler(x(1),ax.XAxis);
+        t2 = num2ruler(x(2),ax.XAxis);
+        [~ ,id1] = min(abs(datenum(t1)-datenum(b(is).times)));
+        [~ ,id2] = min(abs(datenum(t2)-datenum(b(is).times)));
+% 
+%         b(is).x(id1:id2) = linspace(b(is).x(id1-1),b(is).x(id2+1),length(id1:id2));
+%         b(is).y(id1:id2) = linspace(b(is).y(id1-1),b(is).y(id2+1),length(id1:id2));
+%         b(is).z(id1:id2) = linspace(b(is).z(id1-1),b(is).z(id2+1),length(id1:id2));
+
+        b(is).x(id1:id2) = NaN;
+        b(is).y(id1:id2) = NaN;
+        b(is).z(id1:id2) = NaN;
+
+        b(is).x = inpaint_nans(b(is).x,4);
+        b(is).y = inpaint_nans(b(is).y,4);
+        b(is).z = inpaint_nans(b(is).z,4);
         close(gcf);
 
 
-    elseif mmenu == 2
+    elseif mmenu == 3
         tidx = tidx+winlength;
         
         if tidx(end)>b(is).nt
@@ -70,7 +84,7 @@ while 1
         
         close(gcf);
         
-    elseif mmenu == 3
+    elseif mmenu == 4
         
         prompt = {'New Window Length'};
         titles  = '';
@@ -86,7 +100,7 @@ while 1
         
         close(gcf);
         
-    elseif mmenu == 4 %next site
+    elseif mmenu == 5 %next site
         is = is+1;
         
         tidx = 1:winlength;
@@ -101,7 +115,7 @@ while 1
         
         close(gcf);
         
-    elseif mmenu == 5 %previous site
+    elseif mmenu == 6 %previous site
 
         is = is-1;
         
@@ -117,7 +131,7 @@ while 1
         
         close(gcf);
 
-    elseif mmenu == 6 %delete site
+    elseif mmenu == 7 %delete site
 
         table({b.site});
     
@@ -131,7 +145,7 @@ while 1
         close(gcf)
 
         
-    elseif mmenu == 7 %save
+    elseif mmenu == 8 %save
         t0 = b(1).times(1);
         t1 = datetime;
         savestr = [num2str(year(t0)) num2str(month(t0),'%02.f') num2str(day(t0),'%02.f') '_edit_v_',...
